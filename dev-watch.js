63b555ba0d0db2f33ae2932c6ab762d0f8a766c3
fantasyui-com <captain@fantasyui.com>
2018-06-09 10:50:10 -0400

    Build System + Initial UI

---
+++ dev-watch.js
@@ -0,0 +1,166 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+const path = require('path');
+
+const chalk = require('chalk');
+const watch = require('watch');
+
+const posthtml = require('posthtml');
+const posthtmlCustomElements = require('posthtml-custom-elements');
+const minifier = require('posthtml-minifier');
+const minifierOptions = {
+  // collapseWhitespace: true,
+  // preserveLineBreaks: false,
+  // removeComments: true,
+  // minifyCSS: true,
+  // minifyJS: true
+};
+
+function updateHtml(source){
+
+  console.log(chalk.green('Recompiled: '+source));
+
+  const result = posthtml()
+  .use(posthtmlCustomElements())
+  .use(minifier(minifierOptions))
+  .process(fs.readFileSync(source), { sync: true })
+  .html
+
+  //console.log(result)
+  const sourcename = path.basename(source);
+  const basename = path.basename(source, '.src.html');
+  const dirname = path.dirname(source);
+  const destination = path.join(dirname , basename + '.html' )
+  fs.writeFileSync(destination, result + `\n\n <!-- view source of: ${sourcename} -->\n`);
+
+}
+
+function updateCss(source){
+  const synthwave = require('./index.js');
+  const result = synthwave.css(/[a-z]/);
+  fs.writeFileSync(path.join(__dirname, 'style.css'), result);
+}
+
+function updateJs(source){
+  const synthwave = require('./index.js');
+  const result = synthwave.css(/[a-z]/);
+  fs.writeFileSync(path.join(__dirname, 'style.css'), result);
+}
+
+watch.watchTree(__dirname, {filter: function(source){ return source.match(/\.src\.html$/) }, ignoreDirectoryPattern:/node_modules/}, function (f, curr, prev) {
+  if (typeof f == "object" && prev === null && curr === null) {
+    // Finished walking the tree
+    console.log( f );
+    console.log(chalk.yellow( 'HTML Watcher is monitoring:' ));
+    console.log(chalk.yellow( Object.keys(f).filter(i=>i.match(/\.src\.html$/)).join('\n') ));
+    console.log(chalk.yellow( '' ));
+    Object.keys(f).filter(i=>i.match(/\.src\.html$/)).map(i=>updateHtml(i));
+
+  } else if (prev === null) {
+    // f is a new file
+    updateHtml(f);
+
+  } else if (curr.nlink === 0) {
+
+    // f was removed
+    // do nothing updateHtml(f);
+    // user is expected to cleanup
+
+  } else {
+    // f was changed
+    updateHtml(f);
+  }
+})
+watch.watchTree(__dirname, {filter: function(source){ return source.match(/^style\.json$/) }, ignoreDirectoryPattern:/node_modules/}, function (f, curr, prev) {
+  if (typeof f == "object" && prev === null && curr === null) {
+    // Finished walking the tree
+    console.log( f );
+    console.log(chalk.yellow( 'JSON Watcher is monitoring:' ));
+    console.log(chalk.yellow( Object.keys(f).filter(i=>i.match(/^style\.json$/)).join('\n') ));
+    console.log(chalk.yellow( '' ));
+    Object.keys(f).filter(i=>i.match(/^style\.json$/)).map(i=>updateCss(i));
+
+  } else if (prev === null) {
+    // f is a new file
+    updateCss(f);
+
+  } else if (curr.nlink === 0) {
+
+    // f was removed
+    // do nothing updateCss(f);
+    // user is expected to cleanup
+
+  } else {
+    // f was changed
+    updateCss(f);
+  }
+})
+
+
+
+
+const Bundler = require('parcel-bundler');
+
+
+// Entrypoint file location
+
+function updateJs(source){
+
+
+
+  const sourcename = path.basename(source);
+  const basename = path.basename(source, '.src.js');
+  const dirname = path.dirname(source);
+  const destination = path.join(dirname , basename + '.js' )
+
+  // console.log( 'SOURCE: %s', source );
+  // console.log( '  DEST: %s', destination );
+
+  const options = {
+    outDir: '.', // The out directory to put the build files in, defaults to dist
+    outFile: destination, // The name of the outputFile
+
+    publicUrl: './', // The url to server on, defaults to dist
+    watch: false, // whether to watch the files and rebuild them on change, defaults to process.env.NODE_ENV !== 'production'
+    cache: true, // Enabled or disables caching, defaults to true
+    cacheDir: '.parcel-cache', // The directory cache gets put in, defaults to .cache
+    minify: false, // Minify files, enabled if process.env.NODE_ENV === 'production'
+    target: 'browser', // browser/node/electron, defaults to browser
+    https: false, // Server files over https or http, defaults to false
+    logLevel: 3, // 3 = log everything, 2 = log warnings & errors, 1 = log errors
+    hmrPort: 0, // The port the hmr socket runs on, defaults to a random free port (0 in node.js resolves to a random free port)
+    sourceMaps: true, // Enable or disable sourcemaps, defaults to enabled (not supported in minified builds yet)
+    hmrHostname: '', // A hostname for hot module reload, default to ''
+    detailedReport: false // Prints a detailed report of the bundles, assets, filesizes and times, defaults to false, reports are only printed if watch is disabled
+  };
+
+  const bundler = new Bundler(source, options);
+  const bundle = bundler.bundle();
+
+}
+watch.watchTree(__dirname, {filter: function(source){ return source.match(/\.src\.js$/) }, ignoreDirectoryPattern:/node_modules/}, function (f, curr, prev) {
+  if (typeof f == "object" && prev === null && curr === null) {
+    // Finished walking the tree
+    console.log( f );
+    console.log(chalk.yellow( 'JS Watcher is monitoring:' ));
+    console.log(chalk.yellow( Object.keys(f).filter(i=>i.match(/\.src\.js$/)).join('\n') ));
+    console.log(chalk.yellow( '' ));
+    Object.keys(f).filter(i=>i.match(/\.src\.js/)).map(i=>updateJs(i));
+
+  } else if (prev === null) {
+    // f is a new file
+    updateJs(f);
+
+  } else if (curr.nlink === 0) {
+
+    // f was removed
+    // do nothing updateCss(f);
+    // user is expected to cleanup
+
+  } else {
+    // f was changed
+    updateJs(f);
+  }
+})
---
+++ index.html.js
@@ -0,0 +1,280 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+
+// eslint-disable-next-line no-global-assign
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+
+  for (var i = 0; i < entry.length; i++) {
+    newRequire(entry[i]);
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  return newRequire;
+})({3:[function(require,module,exports) {
+
+},{}],5:[function(require,module,exports) {
+/*!
+* screenfull
+* v3.3.2 - 2017-10-27
+* (c) Sindre Sorhus; MIT License
+*/
+(function () {
+	'use strict';
+
+	var document = typeof window !== 'undefined' && typeof window.document !== 'undefined' ? window.document : {};
+	var isCommonjs = typeof module !== 'undefined' && module.exports;
+	var keyboardAllowed = typeof Element !== 'undefined' && 'ALLOW_KEYBOARD_INPUT' in Element;
+
+	var fn = function () {
+		var val;
+
+		var fnMap = [['requestFullscreen', 'exitFullscreen', 'fullscreenElement', 'fullscreenEnabled', 'fullscreenchange', 'fullscreenerror'],
+		// New WebKit
+		['webkitRequestFullscreen', 'webkitExitFullscreen', 'webkitFullscreenElement', 'webkitFullscreenEnabled', 'webkitfullscreenchange', 'webkitfullscreenerror'],
+		// Old WebKit (Safari 5.1)
+		['webkitRequestFullScreen', 'webkitCancelFullScreen', 'webkitCurrentFullScreenElement', 'webkitCancelFullScreen', 'webkitfullscreenchange', 'webkitfullscreenerror'], ['mozRequestFullScreen', 'mozCancelFullScreen', 'mozFullScreenElement', 'mozFullScreenEnabled', 'mozfullscreenchange', 'mozfullscreenerror'], ['msRequestFullscreen', 'msExitFullscreen', 'msFullscreenElement', 'msFullscreenEnabled', 'MSFullscreenChange', 'MSFullscreenError']];
+
+		var i = 0;
+		var l = fnMap.length;
+		var ret = {};
+
+		for (; i < l; i++) {
+			val = fnMap[i];
+			if (val && val[1] in document) {
+				for (i = 0; i < val.length; i++) {
+					ret[fnMap[0][i]] = val[i];
+				}
+				return ret;
+			}
+		}
+
+		return false;
+	}();
+
+	var eventNameMap = {
+		change: fn.fullscreenchange,
+		error: fn.fullscreenerror
+	};
+
+	var screenfull = {
+		request: function (elem) {
+			var request = fn.requestFullscreen;
+
+			elem = elem || document.documentElement;
+
+			// Work around Safari 5.1 bug: reports support for
+			// keyboard in fullscreen even though it doesn't.
+			// Browser sniffing, since the alternative with
+			// setTimeout is even worse.
+			if (/ Version\/5\.1(?:\.\d+)? Safari\//.test(navigator.userAgent)) {
+				elem[request]();
+			} else {
+				elem[request](keyboardAllowed && Element.ALLOW_KEYBOARD_INPUT);
+			}
+		},
+		exit: function () {
+			document[fn.exitFullscreen]();
+		},
+		toggle: function (elem) {
+			if (this.isFullscreen) {
+				this.exit();
+			} else {
+				this.request(elem);
+			}
+		},
+		onchange: function (callback) {
+			this.on('change', callback);
+		},
+		onerror: function (callback) {
+			this.on('error', callback);
+		},
+		on: function (event, callback) {
+			var eventName = eventNameMap[event];
+			if (eventName) {
+				document.addEventListener(eventName, callback, false);
+			}
+		},
+		off: function (event, callback) {
+			var eventName = eventNameMap[event];
+			if (eventName) {
+				document.removeEventListener(eventName, callback, false);
+			}
+		},
+		raw: fn
+	};
+
+	if (!fn) {
+		if (isCommonjs) {
+			module.exports = false;
+		} else {
+			window.screenfull = false;
+		}
+
+		return;
+	}
+
+	Object.defineProperties(screenfull, {
+		isFullscreen: {
+			get: function () {
+				return Boolean(document[fn.fullscreenElement]);
+			}
+		},
+		element: {
+			enumerable: true,
+			get: function () {
+				return document[fn.fullscreenElement];
+			}
+		},
+		enabled: {
+			enumerable: true,
+			get: function () {
+				// Coerce to boolean in case of old WebKit
+				return Boolean(document[fn.fullscreenEnabled]);
+			}
+		}
+	});
+
+	if (isCommonjs) {
+		module.exports = screenfull;
+	} else {
+		window.screenfull = screenfull;
+	}
+})();
+},{}],1:[function(require,module,exports) {
+var project = require("./index.js");
+var screenfull = require("screenfull");
+var nameExpression = new RegExp('^' + project.metadata.name + '-[a-z]$');
+
+$(function () {
+  function renderSynthwave(_ref) {
+    var selectedLetter = _ref.selectedLetter,
+        directedLetter = _ref.directedLetter,
+        selectedClassName = _ref.selectedClassName,
+        directedClassName = _ref.directedClassName;
+
+    $('#demo-body').removeClass(selectedClassName).addClass(directedClassName);
+    $("selected-letter").text(directedLetter);
+    $('.css-code').html(project.css(directedLetter));
+  }
+  function trapNumber(value, min, max) {
+    if (value > max) value = min;
+    if (value < min) value = max;
+    return value;
+  }
+  function getLetter(magic) {
+    var selectedClassName = $('#demo-body').attr('class').split(' ').filter(function (c) {
+      return c.match(nameExpression);
+    })[0] || project.metadata.name + '-' + 'a';
+    var selectedLetter = selectedClassName.split('-')[1];
+    var directedLetterNumber = trapNumber(selectedLetter.charCodeAt(0) + magic, 97, 122);
+    var directedLetter = String.fromCharCode(directedLetterNumber);
+    var directedClassName = project.metadata.name + '-' + directedLetter;
+    var response = { selectedLetter: selectedLetter, directedLetter: directedLetter, selectedClassName: selectedClassName, directedClassName: directedClassName };
+    return response;
+  }
+
+  $('#full-screen').on('click', function () {
+    return screenfull.enabled ? screenfull.request() : "";
+  });
+  $('#aziz-light').on('click', function () {
+    return $('.primary.container').toggleClass('bg-dark').toggleClass('shadow-lg');
+  });
+  $('#prev-wave').on('click', function () {
+    return renderSynthwave(getLetter(-1));
+  });
+  $('#next-wave').on('click', function () {
+    return renderSynthwave(getLetter(1));
+  });
+
+  renderSynthwave(getLetter(0));
+});
+},{"./index.js":3,"screenfull":5}]},{},[1], null)
+//# sourceMappingURL=index.html.map
